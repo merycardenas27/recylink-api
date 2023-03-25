04a7f6812d6edd1db2a6c06ce7b812cd4b64127b
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-03-24 22:55:23 -0500

    build: Set server config

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Allow cross-origin
+app.use(cors());
