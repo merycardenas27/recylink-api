11f132be42eeb77e79283643eaa8c173949d80af
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-03-28 19:16:36 -0500

    graphql(schema): Set root mutation type

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,6 +1,7 @@
 const { GraphQLObjectType, GraphQLSchema } = require('graphql');
 
 const { getBrands } = require('./queries');
+const { createBrand } = require('./mutations');
 
 const QueryType = new GraphQLObjectType({
   name: 'QueryType',
@@ -8,6 +9,15 @@ const QueryType = new GraphQLObjectType({
   fields: { getBrands },
 });
 
+const MutationType = new GraphQLObjectType({
+  name: 'MutationType',
+  description: 'The root mutation type',
+  fields: {
+    createBrand,
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: QueryType,
+  mutation: MutationType,
 });
